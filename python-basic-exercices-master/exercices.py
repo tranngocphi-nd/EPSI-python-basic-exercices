8faeadb69aecb1214061ad9179b33565de8405f8
philaden-nd <tranngocphi.h@gmail.com>
2021-04-05 12:21:36 +0200

    Test ok

--- python-basic-exercices-master/exercices.py
+++ python-basic-exercices-master/exercices.py
@@ -5,7 +5,11 @@ Implémentez les méthodes ci-dessous.
 Pour executer vos tests il vous faudra utiliser pytest
 """
 
-def htc_to_ttc(htc_cost: float, discount_rate: float = 0) -> float :
+
+import math
+
+
+def htc_to_ttc(htc_cost: float, discount_rate: float = 0) -> float:
     """
     Exercice 1 :
     Calcule le coût TTC d'un produit.
@@ -13,6 +17,11 @@ def htc_to_ttc(htc_cost: float, discount_rate: float = 0) -> float :
     Taux de taxes : 20.6 %
     Retourne un float arrondi à deux décimales
     """
+    if discount_rate < 0 or discount_rate > 1:
+        raise Exception
+    ttc_cost = round(htc_cost * 1.206 * (1 - discount_rate), 2)
+    return ttc_cost
+
 
 def divisors(value: int = 0):
     """
@@ -21,3 +30,52 @@ def divisors(value: int = 0):
     retourne ses diviseurs (sans répétition)
     s’il y en a, ou « PREMIER » s’il n’y en a pas.
     """
+
+    """
+    PRIME_NUMBER_LIST = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+                         101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,
+                         211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293,
+                         307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397,
+                         401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499,
+                         503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599,
+                         601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691,
+                         701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
+                         809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887,
+                         907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997,]
+    """
+
+    divisors_set = []
+
+    initial_value = value
+
+    if value == 0:
+        return "All integer numbers different from 0 are divisors of 0"
+    elif value == 1:
+        return "ONE"
+    elif value < 0:
+        return "The value should be positive integer"
+    else:
+        # if the number is 2
+        if value == 2:
+            return "PREMIER"
+        else:
+            # number of two's that divide n
+            while value % 2 == 0:
+                divisors_set.append(2)
+                value = value / 2
+
+            # n must be odd at this point
+            # so a skip of 2 ( i = i + 2) can be used
+            for i in range(3, int(math.sqrt(value)) + 1, 2):
+                # while i divides n , print i ad divide n
+                while value % i == 0:
+                    divisors_set.append(i)
+                    value = value / i
+
+            if 2 < value < initial_value:
+                divisors_set.append(value)
+
+    if len(divisors_set) == 0:
+        return "PREMIER"
+    else:
+        return set(divisors_set)
--- python-basic-exercices-master/test_exercices.py
+++ python-basic-exercices-master/test_exercices.py
@@ -18,13 +18,13 @@ class MyTestCase(unittest.TestCase):
 
     def test_htc_to_ttc_with_invalid_discount(self):
         """ If incorrect parameters given. The function generates an Exception. """
-        self.skipTest("Exercice 1 : Validate the to previous methods before...")
+        #self.skipTest("Exercise 1 : Validate the to previous methods before...")
         with self.assertRaises(Exception) as e_info:
             htc_to_ttc(10, 2)
         with self.assertRaises(Exception) as e_info:
             htc_to_ttc(10, -0.2)
 
-    @unittest.skip("Exercice 2")
+    #@unittest.skip("Exercise 2")
     def test_divisors(self):
         """Find the prime dividers of a number. """
         self.assertEqual({3,5}, divisors(15))
@@ -32,13 +32,13 @@ class MyTestCase(unittest.TestCase):
 
     def test_divisors_prime_number(self):
         """ Special case if number is already a prime number """
-        self.skipTest("Exercice 2 : Validate the previous step...")
+        #self.skipTest("Exercise 2 : Validate the previous step...")
         self.assertEqual('PREMIER', divisors(2))
         self.assertEqual('PREMIER', divisors(97))
 
     def test_divisors_multiple(self):
         """ Special case, do not return duplicates """
-        self.skipTest("Exercice 2 : Validate the previous step...")
+        #self.skipTest("Exercise 2 : Validate the previous step...")
         self.assertEqual({2}, divisors(4))
         self.assertEqual({3,5,7}, divisors(525))
 
